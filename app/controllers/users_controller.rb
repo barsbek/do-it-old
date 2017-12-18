978bf5c04ee0b38b0c2c27cf016b4d325657075e
barsbek <mak.bekmamat@gmail.com>
2017-12-19 04:25:24 +0500

    user can authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def login
+  end
+
+  def authenticate
+    @user = User.find_by_email(params[:email])
+    respond_to do |format|
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        format.html { redirect_to @user, notice: 'User has been successfully logged in' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { redirect_to login_url, alert: 'Invalid email or password' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    respond_to do |format|
+      format.html { redirect_to login_url, notice: 'Logged out' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get :login, to: 'users#login'
+  post :login, to: 'users#authenticate'
+  get :logout, to: 'users#logout'
 end
