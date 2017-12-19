c25aa8df188c4bd8ce1194387eeace9764ccb365
barsbek <mak.bekmamat@gmail.com>
2017-12-19 07:30:57 +0500

    scaffold task

---
+++ db/migrate/20171219023010_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.integer :priority
+      t.integer :order
+      t.references :collection, foreign_key: true
+      t.references :list, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
