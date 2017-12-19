6560708d3f594fe3a08c0a6781167b1862f4d4c3
barsbek <mak.bekmamat@gmail.com>
2017-12-19 07:14:28 +0500

    validate user params

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: {maximum: 250},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates_presence_of :name, :email
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @user = User.new(new_user_params)
+  end
+
+  test "with valid params" do
+    assert @user.valid?
+  end
+
+  test "shouldn't save without email" do
+    @user.email = ""
+    refute @user.save
+  end
+
+  test "shouldn't save without name" do
+    @user.name = ""
+    refute @user.save
+  end
+
+  test "shouldn't save invalid email" do
+    @user.email = "myString"
+    refute @user.save
+  end
 end
